5dc4c47d330b1f1c2e33a70cd5fae9a58edaad33
thalesCastellani <62717114+thalesCastellani@users.noreply.github.com>
2022-05-01 21:44:43 -0300

    Incluindo validacoes de campos email, cpf e telefone via regex

--- src/main/java/br/com/fiap/brq/controller/CandidatoController.java
+++ src/main/java/br/com/fiap/brq/controller/CandidatoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.transaction.Transactional;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -30,88 +31,85 @@ public class CandidatoController {
 
 	@Autowired
 	private CandidatoRepository candidatoRepository;
-	
+
 	@GetMapping
 	public List<Candidato> lista() {
 		List<Candidato> candidatos = candidatoRepository.findAll();
 		return candidatos;
 	}
-	
+
 	@GetMapping(value = "/skill/{skill}")
 	public ResponseEntity<Page<Candidato>> buscaInfosCandidatoPorSkill(@PathVariable String skill) {
-		
-		Pageable paginacao = PageRequest.of(0, 10, Sort.by(
-				List.of(
-						new Sort.Order(Sort.Direction.ASC, "skill"),
+
+		Pageable paginacao = PageRequest.of(0, 10,
+				Sort.by(List.of(new Sort.Order(Sort.Direction.ASC, "skill"),
 						new Sort.Order(Sort.Direction.DESC, "certificacao", Sort.NullHandling.NULLS_LAST),
-						new Sort.Order(Sort.Direction.ASC, "nome")
-						)
-				));
-		
+						new Sort.Order(Sort.Direction.ASC, "nome"))));
+
 		Page<Candidato> candidatos = candidatoRepository.findBySkill_Nome(skill, paginacao);
-		
+
 		if (!candidatos.isEmpty()) {
 			return ResponseEntity.ok(candidatos);
 		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
-		
+
 	@GetMapping(value = "/nome/{nome}")
 	public ResponseEntity<Candidato> buscaInfosCandidatoPorNome(@PathVariable String nome) {
 		Optional<Candidato> candidato = candidatoRepository.findByNome(nome);
-		
+
 		if (candidato.isPresent()) {
 			return ResponseEntity.ok(candidato.get());
 		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@GetMapping(value = "/email/{email}")
 	public ResponseEntity<Candidato> buscaInfosCandidatoPorEmail(@PathVariable String email) {
 		Optional<Candidato> candidato = candidatoRepository.findByEmail(email);
-		
+
 		if (candidato.isPresent()) {
 			return ResponseEntity.ok(candidato.get());
 		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@GetMapping(value = "/cpf/{cpf}")
 	public ResponseEntity<Candidato> buscaInfosCandidatoPorCpf(@PathVariable String cpf) {
 		Optional<Candidato> candidato = candidatoRepository.findByCpf(cpf);
-		
+
 		if (candidato.isPresent()) {
 			return ResponseEntity.ok(candidato.get());
 		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@GetMapping(value = "/certificacao/{certificacao}")
 	public ResponseEntity<Candidato> buscaInfosCandidatoPorCertificacao(@PathVariable String certificacao) {
 		Optional<Candidato> candidato = candidatoRepository.findByCertificacao(certificacao);
-		
+
 		if (candidato.isPresent()) {
 			return ResponseEntity.ok(candidato.get());
 		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
-	
+
 	@PostMapping
 	@Transactional
-	public ResponseEntity<Candidato> cadastra(@RequestBody CandidatoDto candidatoDto, UriComponentsBuilder uriComponentsBuilder) {
-		
+	public ResponseEntity<Candidato> cadastra(@RequestBody @Valid CandidatoDto candidatoDto,
+			UriComponentsBuilder uriComponentsBuilder) {
+
 		Candidato candidato = candidatoDto.converte(candidatoDto);
-		
+
 		candidatoRepository.save(candidato);
-		
+
 		URI uri = uriComponentsBuilder.path("/candidato/{id}").buildAndExpand(candidato.getId()).toUri();
 		return ResponseEntity.created(uri).body(candidato);
 	}
-	
 
 }
--- src/main/java/br/com/fiap/brq/dto/CandidatoDto.java
+++ src/main/java/br/com/fiap/brq/dto/CandidatoDto.java
@@ -3,6 +3,8 @@ package br.com.fiap.brq.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.validation.constraints.Pattern;
+
 import br.com.fiap.brq.model.Candidato;
 import br.com.fiap.brq.model.Genero;
 import br.com.fiap.brq.model.Skill;
@@ -22,8 +24,11 @@ public class CandidatoDto {
 	}
 
 	private String nome;
+	@Pattern(regexp = "^([0-9]{3}\\.?){3}-?[0-9]{2}$", message = "Deve seguir o padrão: 000.000.000-00")
 	private String cpf;
+	@Pattern(regexp = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$", message = "Deve seguir o padrão: nome@email.com")
 	private String email;
+	@Pattern(regexp = "^((\\(\\d{2}\\))|\\d{2})[- .]?\\d{5}[- .]?\\d{4}$", message = "Deve seguir o padrão: (11) 99999-9999")
 	private String telefone;
 	private Genero genero;
 	private LocalDateTime dataNascimento;
@@ -70,10 +75,10 @@ public class CandidatoDto {
 		String telefone = candidatoDto.getTelefone();
 		Genero genero = candidatoDto.getGenero();
 		LocalDateTime dataNascimento = candidatoDto.getDataNascimento();
-		
+
 		List<SkillDto> skillDto = candidatoDto.getSkill();
 		List<Skill> skills = Skill.converte(skillDto);
-		
+
 		String certificacao = candidatoDto.getCertificacao();
 
 		Candidato candidato = new Candidato();
